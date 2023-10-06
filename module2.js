a7df3e3849b2574c58b89a3546ad269013b93e98
pmatnz <paullmat@gmail.com>
2023-10-06 15:35:53 +1300

    Added popping animation to icons and nav bar heading

---
+++ module2.js
@@ -0,0 +1,12 @@
+var namyName ="Paul"; //this is a javascript variable
+/* if you declare a variable with no value it will be undefined, if you do an operation to an undefined variable it will return NaN = not a number*/
+
+let dogsName = "Max"; // a let variable can only be declared once unlike usine var, best common pratice to use let in javascript
+
+const catsName = "Maisy" /* using const has the same impact as let but it can no longer be charged again, it will retain "Maisy" forver unless const is changed, 
+This means they are unmutable , can't be changed. Common practice is to also name any const variables in all capitals const MYVAR */
+
+myVar = 10; myVar++; // ++ will add 1 to the original value of myVar, same with -- it will takeaway 1
+
+
+
